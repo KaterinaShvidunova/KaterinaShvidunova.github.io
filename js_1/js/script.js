e0a9fc8040e7b56f8fa8c7a6a8fb64163ea4b501
KaterinaShvidunova <katya92katerlna@gmail.com>
2016-08-01 23:22:04 +0300

    js_1

---
+++ js_1/js/script.js
@@ -0,0 +1,51 @@
+function number() {
+    var a = prompt('Введите число');
+    var b = prompt('Введите степень');
+    if(!a || !b){
+        alert('Введите корректные данные');
+        number();
+    } else {
+        pow(a,b);
+    }
+}
+function pow(a,b) {
+    var result=a;
+    if(b>0){
+    for (var i=1; i<b; i++){
+        result = result*a;
+    }
+    } else if(b==0){
+        result = 1;
+    } else {
+        for (var i=1; i<-b; i++){
+        result = result*a;
+    } 
+        result=1/result;
+    }
+    alert(result);
+}
+
+function authorization() {
+    var arr = [];
+    for (var i=1; i<6; i++){
+      arr[i]=prompt('Имя '+i);
+    }
+    var yourName = prompt('Введите ваше имя');
+    var yourNameLowerCase =yourName.toLowerCase();
+    var arrItem;
+        var i=1;
+    do {
+        arrItem = arr[i].toLocaleLowerCase();
+        if(arrItem==yourNameLowerCase && arr[i]!==''){
+            alert('Вы успешно прошли авторизацию, '+yourName+'!');
+            break;
+        } else if(i<5){
+         i++;
+        } else {
+        alert('Ошибка авторизации');
+            break;
+        }
+        
+    } while (i<6);
+    
+}
